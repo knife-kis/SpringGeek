c00d49664c6c2d3328d8cb3464c1b1263715a206
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-10-19 20:08:02 +0700

    hw08 task 1.2.3.4

--- Optima/src/main/java/ru/laboratoriyaoptima/persist/entity/User.java
+++ Optima/src/main/java/ru/laboratoriyaoptima/persist/entity/User.java
@@ -14,34 +14,34 @@ import java.util.List;
 @NoArgsConstructor
 public class User {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private Integer id;
-//
-//    @Column(name = "firstName")
-//    private String firstName;
-//
-//    @Column(name = "lastName")
-//    private String lastName;
-//
-//    @Column(name = "password")
-//    private String password;
-//
-//    @Column(name = "email")
-//    private String email;
-//
-//    @Column(name = "phone")
-//    private String phone;
-//
-//    @ManyToMany
-//    @JoinTable(name = "users_roles",
-//            joinColumns = @JoinColumn(name = "user_id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id"))
-//    private Collection<Role> roles;
-//
-//    public String getFullName() {
-//        return String.format("%s %s", firstName, lastName);
-//    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
 
 }
