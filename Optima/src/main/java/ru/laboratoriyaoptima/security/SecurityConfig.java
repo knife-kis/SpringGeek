d1e79e888cb0d1e2e19a5b4d89c0b87f1d4fd731
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-10-19 20:05:31 +0700

    /

--- Optima/src/main/java/ru/laboratoriyaoptima/security/SecurityConfig.java
+++ Optima/src/main/java/ru/laboratoriyaoptima/security/SecurityConfig.java
@@ -10,42 +10,42 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.laboratoriyaoptima.persist.services.UsersService;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private UsersService usersService;
-
-    @Autowired
-    public void setUserService(UsersService usersService) {
-        this.usersService = usersService;
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/equipment/**").authenticated()
-                .antMatchers("/request/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/authenticate")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .permitAll();
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(usersService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
-}
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private UsersService usersService;
+//
+//    @Autowired
+//    public void setUserService(UsersService usersService) {
+//        this.usersService = usersService;
+//    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/equipment/**").authenticated()
+//                .antMatchers("/request/**").hasRole("ADMIN")
+//                .anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/authenticate")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .permitAll();
+//    }
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+//        auth.setUserDetailsService(usersService);
+//        auth.setPasswordEncoder(passwordEncoder());
+//        return auth;
+//    }
+//}
